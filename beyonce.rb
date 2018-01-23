9f69853b15ee1d36c576d65a3fc4de57bc935ab1
Sarah Yee <sarahyee41@gmail.com>
2018-01-23 00:32:55 +0000

    automatically backed up by learn

--- beyonce.rb
+++ beyonce.rb
@@ -1,1 +1,3 @@
-puts "There are num_of_attendees people coming to Beyonce's birthday party."
+num_of_attendees = 547
+
+puts "There are #{num_of_attendees} people coming to Beyonce's birthday party."
